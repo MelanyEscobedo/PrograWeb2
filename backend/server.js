f54cdec83d61d18805a482734bce2ab4ad677ee2
BillowyExpert92 <diego_nightmare@outlook.com>
2026-05-19 03:38:29 -0600

    Prueba de hosteo

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/animareview";
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
 app.use(express.json());
 const path = require("path");
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
